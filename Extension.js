eb07e904e7ca95c004bff0aa671b140467cb0287
Caleb Jeffery <jeffdev.mgmt@gmail.com>
2024-04-17 23:04:26 +0000

    Extension files

---
+++ Extension.js
@@ -0,0 +1,76 @@
+let myLeads = []
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.querySelector(".delete-btn")
+const tabBtn = document.getElementById("saveTab-btn")
+
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+
+if (leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+ chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  
+  myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+
+ })
+})
+
+
+
+
+
+function render(leads){
+  let listItems = "" 
+
+
+
+  for(let i=0; i < leads.length; i ++){
+  //listItems += "<li><a href = '"+ myLeads[i] +"' target = _'blank>"+ myLeads[i] +"</a></li>"
+  listItems +=`
+  <li>
+    <a href = '${ leads[i]}' target = _'blank'>${ leads[i]}</a>
+  </li>
+  `
+}
+ulEl.innerHTML = listItems
+
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+  
+})
+
+
+
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  render(myLeads)
+  console.log(localStorage.getItem("myLeads"))
+});
+
+
+
+
+//or
+//const li = document.createElement("li")
+//li.textContent = myLeads[i]
+//ulEl.append(li)
+
+
